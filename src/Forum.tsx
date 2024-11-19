04110b8e56df1175de3d86125c9da73070444ca8
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-11-19 01:13:20 -0700

    Update Forum.tsx

--- src/Forum.tsx
+++ src/Forum.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 const client = generateClient<Schema>();
 const { forumId } = useParams<{ forumId: string }>();
 const {data : forum} = await client.models.Forum.get({ id: forumId });
-const {data : posts} = await forum?.posts();
+const {data : posts} = await forum.posts();
 
 function Forum() {
     
@@ -13,21 +13,22 @@ function Forum() {
       <main>
         <button>Add New Post</button>
         <h1>Forum: {forum?.name}</h1>
-  
+        <ul>
         {posts.length ? (
-          posts.map((post) => (
+          posts.forEach((post: { id: any; subject: any; content: any; createdAt: string | number | Date; }) => {
             <div key={post.id} style={{ marginBottom: '15px', padding: '10px', border: '1px solid #ccc' }}>
               <h2>{post.subject}</h2>
               <p>{post.content}</p>
               <small>Posted by: PLACEHOLDER on {new Date(post.createdAt).toLocaleDateString()}</small>
             </div>
-          ))
+          })
         ) : (
           <div>
             <h1>There are no posts... yet.</h1>
             <h2>Be the first!</h2>
           </div>
         )}
+        </ul>
       </main>
     );
 }
