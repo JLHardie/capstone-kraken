6c87b85ac0c961e74a410b0f6fbca4bb9ee6a576
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-11-19 01:45:18 -0700

    Update Forum.tsx

--- src/Forum.tsx
+++ src/Forum.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 const client = generateClient<Schema>();
 const { forumId } = useParams<{ forumId: string }>();
 const { data: forum } = await client.models.Forum.get({ id: forumId });
-const posts = await forum?.posts();
+const { data: posts } = await forum?.posts();
 
 function Forum() {
     
