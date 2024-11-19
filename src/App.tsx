86d4e2b56ad8f97051d15efddda2476a599c39cd
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-11-18 22:20:31 -0700

    Added Homepage
    root should automatically reroute to the /feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Forum from "./Forum";
 import Post from "./Post";
 import SearchBar from './SearchBar';
 import Search from "./Search";
+import Home from "./Home";
 import { Authenticator } from '@aws-amplify/ui-react'
 //import '@aws-amplify/ui-react/styles.css'
 
@@ -16,10 +17,11 @@ function App() {
           <h1>Kraken</h1>
           <SearchBar />
           <Routes>
+            <Route path="/feed" element={<Home />} />
             <Route path="/forum/:forumid" element={<Forum />} />
             <Route path="/post/:postid" element={<Post />} />
             <Route path="/search" element={<Search />} />
-            <Route path="/" element={<h2>Welcome! Select a page:</h2>} />
+            <Route path="/" element={<Navigate to="/feed" replace />} />
           </Routes>
         </div>
       </Router>
---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+
+
+    return (
+        <div>
+            <h2>Welcome to the Feed</h2>
+        </div>
+    )
+}
+
+export default Home;
