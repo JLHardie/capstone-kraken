281209f8680776a118f167fcb9b27a06667d4947
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-11-19 20:55:13 -0700

    Reverted last change

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -62,7 +62,7 @@ function CreatePost() {
                 containsImage: false, // Set default value for now
                 datePosted: new Date().toISOString(),
                 likes: 0, // Default likes count
-                forum: forum, // Link to the forum by ID
+                forumid: forumId, // Link to the forum by ID
             });
 
             console.log("Post created:", post);
