0d63bf337c5c7de241c57baaac626db9eb2a2e6e
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-11-19 20:42:38 -0700

    ForumId to Forum
    Changing it from creating post linked by forumid to just the forum

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -62,7 +62,7 @@ function CreatePost() {
                 containsImage: false, // Set default value for now
                 datePosted: new Date().toISOString(),
                 likes: 0, // Default likes count
-                forumid: forum.id, // Link to the forum by ID
+                forum: forum, // Link to the forum by ID
             });
 
             console.log("Post created:", post);
