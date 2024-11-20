71fbcf5c2c8d8abedd48dbbbe68204fc97a9ff9a
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-11-19 22:14:31 -0700

    Reworking Post Page

--- src/Post.tsx
+++ src/Post.tsx
@@ -7,50 +7,41 @@ const client = generateClient<Schema>();
 
 function Post() {
   const { postId } = useParams<{ postId: string }>();
-  const [posts, setPosts] = useState<Array<Schema["Post"]["type"]>>([]);
-  const [comments, setComments] = useState<Array<Schema["Comment"]["type"]>>([]);
+
+  if (!postId) {
+    throw new Error("Invalid postId.")
+  }
+
+  const [post, setPost] = useState<Schema['Post']['type'] | null>(null);
+  const [comments, setComments] = useState<Schema['Comment']['type'][]>([]);
 
   useEffect(() => {
-    const postSubscription = client.models.Post.observeQuery().subscribe({
-        next: async (data) => setPosts(await data.items.filter(post => post.id === postId)),
-        error: (error) => console.error("Error fetching posts:", error),
-      });
+    const fetchData = async () => {
+      const { data: postData } = await client.models.Post.get({id: postId})
+      if (!postData) {
+        throw new Error("Post not found");
+      }
+      setPost(postData);
 
-    const commentSubscription = client.models.Comment.observeQuery().subscribe({
-      next: async (data) => {
-        const filteredComments = await Promise.all(data.items.map(async (comment) => {
-            const post = await comment.post();
-            if (post && post.data?.id === postId) {
-              return comment;
-            }
-            return null;
-        }))
-        setComments(filteredComments.filter(comment => comment !== null));
-      },
-      error: (error) => console.error("Error fetching comments:", error),
-    });
-    return () => {
-        commentSubscription.unsubscribe();
-        postSubscription.unsubscribe();
-      };
+      const { data: commentsData } = await client.models.Comment.list({
+        filter: {
+          postid : { eq : postId }
+        }
+      })
+      if (!commentsData) {
+        throw new Error("Comments not found");
+      }
+      setComments(commentsData);
+    }
+    fetchData();
   }, [postId]);
 
 
   return (
     <div>
-        {
-            posts.length ? (
-                posts.map((post) => (
-                    <div>
-                    <h1>{post.subject}</h1>
-                    <p>{post.content}</p>
-                    <small>Posted on: {(post.datePosted)}</small>
-                    </div>
-                ))
-            ) : (
-                <h2>Post data failed to load.</h2>
-            )
-        }
+      <h1>{post.subject}</h1>
+      <p>{post.content}</p>
+      <small>Posted on: {post.datePosted}</small>
       <div>
         <h3>Comments</h3>
         {comments.length ? (
