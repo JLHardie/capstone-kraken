7301441ccaaaa59a60132d5ef2104f8f031cf224
VIROS <jhardie@student.neumont.edu>
2024-11-20 23:07:26 -0700

    added message bar to chatrooms

--- src/Chatroom.tsx
+++ src/Chatroom.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '../amplify/data/resource';
 import { useParams } from "react-router-dom";
+import { getCurrentUser } from 'aws-amplify/auth';
 
 type Message = Schema['Message']['type'];
 type Forum = Schema['Forum']['type'];
@@ -9,6 +10,7 @@ const client = generateClient<Schema>();
 
 export default function Chatroom() {
     const [messages, setMessages] = useState<Message[]>([]);
+    const [newMessage, setNewMessage] = useState('');
     const [forum, setForum] = useState<Forum>();
     const { forumId } = useParams<{forumId : string}>()
     if (!forumId) {
@@ -35,6 +37,21 @@ export default function Chatroom() {
         return () => sub.unsubscribe();
     }, []);
 
+    const handleNewComment = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const { username } = await getCurrentUser();
+        if ((newMessage.trim() !== '') && !(!forum)) {
+            const { data: newMessageData } = await client.models.Message.create({
+              content: newMessage,
+              sender: username,
+              forumId: forum.id
+            })
+    
+            console.log(newMessageData)
+            setNewMessage('');
+        }
+      };
+
     return (
         <div>
             <h2>{}</h2>
@@ -49,6 +66,15 @@ export default function Chatroom() {
                     ))
                 }
             </ul>
+                <form onSubmit={handleNewComment}>
+                    <input
+                        type="text"
+                        placeholder="Send Message..."
+                        value={newMessage}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewMessage(e.target.value)}
+                    />
+                    <button type="submit">Comment</button>
+                </form>
         </div>
     )
 }
