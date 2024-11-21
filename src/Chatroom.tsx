29e3d9f00f92514f6ef36ee3829d8c91894aa2e5
VIROS <jhardie@student.neumont.edu>
2024-11-21 04:55:36 -0700

    size adjustments

--- src/Chatroom.tsx
+++ src/Chatroom.tsx
@@ -58,7 +58,7 @@ export default function Chatroom() {
             <h2>{}</h2>
             <div>
                 <ul>
-                    <ScrollView height="60%" width="40%" autoScroll='auto'>
+                    <ScrollView height="500px" width="250px" autoScroll='auto'>
                     {
                         messages.map((message) => (
                             (message.forumid === forumId) ? (
