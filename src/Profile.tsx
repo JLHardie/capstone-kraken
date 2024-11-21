98f51babed8cddceb3adc928442c43cb65152bae
VIROS <jhardie@student.neumont.edu>
2024-11-21 05:28:32 -0700

    theme testing

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,4 +1,4 @@
-import { AccountSettings } from '@aws-amplify/ui-react';
+import { AccountSettings, ThemeProvider } from '@aws-amplify/ui-react';
 import { signOut } from 'aws-amplify/auth';
 import '@aws-amplify/ui-react/styles.css';
 
@@ -8,18 +8,39 @@ export default function Profile() {
         await signOut()
     }
 
+    const theme = {
+        name: 'custom-theme',
+        tokens: {
+          colors: {
+            border: {
+              primary: { value: '{colors.neutral.40}' },
+              secondary: { value: '{colors.neutral.20}' },
+              tertiary: { value: '{colors.neutral.10}' },
+            },
+          },
+          radii: {
+            small: { value: '2px' },
+            medium: { value: '3px' },
+            large: { value: '4px' },
+            xl: { value: '6px' },
+          },
+        },
+      };
+
     return (
         <div>
-            <h2>Profile</h2>
-            <button type="button" onClick={doSignOut}>Sign Out</button>
-            <AccountSettings.ChangePassword 
-                displayText={{
-                    currentPasswordFieldLabel: 'Enter current password',
-                    newPasswordFieldLabel: 'Enter new password',
-                    confirmPasswordFieldLabel: 'Confirm your password',
-                    updatePasswordButtonText: 'Update your password',
-                }}  
-            />
+            <ThemeProvider theme={theme}>
+                <h2>Profile</h2>
+                <button type="button" onClick={doSignOut}>Sign Out</button>
+                <AccountSettings.ChangePassword 
+                    displayText={{
+                        currentPasswordFieldLabel: 'Enter current password',
+                        newPasswordFieldLabel: 'Enter new password',
+                        confirmPasswordFieldLabel: 'Confirm your password',
+                        updatePasswordButtonText: 'Update your password',
+                    }}  
+                />
+            </ThemeProvider>
         </div>
     )
 }
