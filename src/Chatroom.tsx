f838ed56c09055485877dbcb02347cd4504e53ec
VIROS <jhardie@student.neumont.edu>
2024-11-21 04:43:27 -0700

    autoscroll for chat

--- src/Chatroom.tsx
+++ src/Chatroom.tsx
@@ -58,7 +58,7 @@ export default function Chatroom() {
             <h2>{}</h2>
             <div>
                 <ul>
-                    <ScrollView height="300px">
+                    <ScrollView height="300px" autoScroll='auto'>
                     {
                         messages.map((message) => (
                             (message.forumid === forumId) ? (
