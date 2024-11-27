35a00a41117c640b70fd5a5f571071819fb8cb3f
VIROS <jhardie@student.neumont.edu>
2024-11-27 15:27:28 -0700

    testing auth services

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,21 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react'
+import { signUp, SignUpInput } from "aws-amplify/auth";
 
 Amplify.configure(outputs);
 
+const services = {
+  async handleSignUp(input: SignUpInput) {
+    console.log(input);
+    // custom username and email
+    return signUp(input);
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator services={services}>
       <App />
     </Authenticator>
   </React.StrictMode>
