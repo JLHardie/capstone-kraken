d0888ea48fa19737f4112fe3e67991b985a7d47e
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-12-03 18:49:02 -0700

    reversions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
 import Forum from "./Forum";
 import Post from "./Post";
+import SearchBar from './SearchBar';
 import Search from "./Search";
 import Home from "./Home";
 import CreatePost from "./CreatePost";
@@ -9,31 +10,46 @@ import Profile from "./Profile";
 import DM from "./DM";
 import MessageHub from "./MessageHub";
 import { Button, Card, Flex, Image } from "@aws-amplify/ui-react";
-import Navbar from "./Navbar";
 //import '@aws-amplify/ui-react/styles.css'
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/feed" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/forum/:forumId" element={<Forum />} />
-            <Route path="/forum/:forumId/create" element={<CreatePost />} />
-            <Route path="/post/:postId" element={<Post />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/forum/:forumId/chat" element={<Chatroom />} />
-            <Route path="/dm/:dmId" element={<DM />} />
-            <Route path="/dm" element={<MessageHub />} />
-            <Route path="/" element={<Navigate to="/feed" replace />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <div>
+      
+      <Card className="navbar-card">
+        <Flex
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          alignContent="flex-start"
+          wrap="nowrap"
+          gap="1rem"
+        >
+          <Link to={`/feed`}>
+            <Image alt="Kraken Logo" src="/Kraken.png" height="75px" width="75px" />
+          </Link>
+          <SearchBar />
+          <Link to={`/profile`}>
+            <Button>Profile</Button>
+          </Link>
+        </Flex>
+      </Card>
+        <Routes>
+          <Route path="/feed" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forum/:forumId" element={<Forum />} />
+          <Route path="/forum/:forumId/create" element={<CreatePost />} />
+          <Route path="/post/:postId" element={<Post />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/forum/:forumId/chat" element={<Chatroom />} />
+          <Route path="/dm/:dmId" element={<DM />} />
+          <Route path="/dm" element={<MessageHub />} />
+          <Route path="/" element={<Navigate to="/feed" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
