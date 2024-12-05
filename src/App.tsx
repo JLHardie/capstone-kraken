e16e194afed93d035c5b44a08430a6da018ea619
VIROS <123793529+JLHardie@users.noreply.github.com>
2024-12-04 20:12:26 -0700

    Style Testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Chatroom from "./Chatroom";
 import Profile from "./Profile";
 import DM from "./DM";
 import MessageHub from "./MessageHub";
-import { Button, Flex, Image, Menu, MenuItem, View } from "@aws-amplify/ui-react";
+import { Button, Card, Flex, Grid, Image, Menu, MenuItem, View } from "@aws-amplify/ui-react";
 //import '@aws-amplify/ui-react/styles.css'
 
 export default function App() {
@@ -17,51 +17,69 @@ export default function App() {
 
   return (
     <Router>
-      <Flex
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="center"
-        alignContent="flex-start"
-        wrap="nowrap"
-        gap="1rem"
-        id="navbar-card"
+      <Grid
+        columnGap="0.5rem"
+        rowGap="0.5rem"
+        templateColumns=".5fr 1fr 1fr"
+        templateRows="1fr 3fr"
       >
-        <View as="div" height="100%">
-          <Link to={'/'}>
-            <Image 
-              alt="Kraken Logo" 
-              src="/Kraken.png" 
-              height="100%"
-              objectFit="fill" />
-          </Link>
-          <SearchBar />
-        </View>
-        <Menu size="large" menuAlign="center">
-          <MenuItem>Subscritions</MenuItem>
-          <MenuItem>
-            <Link to={'/dm'}>
-              Direct Messages
+        <Flex
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          alignContent="flex-start"
+          wrap="nowrap"
+          gap="1rem"
+          columnStart="1"
+          columnEnd="-1"
+        >
+          <View as="div" height="100%" >
+            <Link to={'/'}>
+              <Image 
+                alt="Kraken Logo" 
+                src="/Kraken.png" 
+                height="100%"
+                objectFit="fill" />
             </Link>
-          </MenuItem>
-        </Menu>
-        <Link to={`/profile`}>
-          <Button variation="primary">Profile</Button>
-        </Link>
-      </Flex>
-      <main>
-        <Routes>
-          <Route path="/feed" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/forum/:forumId" element={<Forum />} />
-          <Route path="/forum/:forumId/create" element={<CreatePost />} />
-          <Route path="/post/:postId" element={<Post />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/forum/:forumId/chat" element={<Chatroom />} />
-          <Route path="/dm/:dmId" element={<DM />} />
-          <Route path="/dm" element={<MessageHub />} />
-          <Route path="/" element={<Navigate to="/feed" replace />} />
-        </Routes>
-      </main>
+            <SearchBar />
+          </View>
+          <Menu size="large" menuAlign="center">
+            <MenuItem>Subscritions</MenuItem>
+            <MenuItem>
+              <Link to={'/dm'}>
+                Direct Messages
+              </Link>
+            </MenuItem>
+          </Menu>
+          <Link to={`/profile`}>
+            <Button variation="primary">Profile</Button>
+          </Link>
+        </Flex>
+        <Card
+          columnStart="1"
+          columnEnd="2"
+        >
+          Nav
+        </Card>
+        <View
+          as="div"
+          columnStart="2"
+          columnEnd="-1"
+        >
+          <Routes>
+            <Route path="/feed" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/forum/:forumId" element={<Forum />} />
+            <Route path="/forum/:forumId/create" element={<CreatePost />} />
+            <Route path="/post/:postId" element={<Post />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/forum/:forumId/chat" element={<Chatroom />} />
+            <Route path="/dm/:dmId" element={<DM />} />
+            <Route path="/dm" element={<MessageHub />} />
+            <Route path="/" element={<Navigate to="/feed" replace />} />
+          </Routes>
+        </View>
+      </Grid>
     </Router>
   );
 }
